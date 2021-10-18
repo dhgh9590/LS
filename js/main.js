f083b76e06266d4f2c60a08d9c134e677f5eae25
hoseol oh <dhgh9590@naver.com>
2021-10-18 16:59:46 +0900

    Second commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $(".family").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("click");
+        $(this).find(".sub_menu_list").slideToggle(300);
+        $(this).find(".sub_menu_list li a").css({color : "#fff"})
+    });
+
+    $(".menu > ul > li").mouseenter(function(){
+        $(this).find(".menu_list").stop().slideDown(400);
+        $(this).addClass("active");
+    })
+    .mouseleave(function(){
+        $(this).find(".menu_list").stop().slideUp(300);
+        $(this).removeClass("active");
+    });
+
+    $(window).scroll(function(){
+        let scrollTop = $(window).scrollTop();
+            if(scrollTop != 0){
+                $("#nav").addClass("active");
+            }else{
+                $("#nav").removeClass("active");
+            }
+    });
+
+});
